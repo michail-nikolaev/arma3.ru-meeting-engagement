52d59ff33a068255f18ab51fe6d5a086911ac8f1
Michail Nikolaev <michail.nikolaev@gmail.com>
2013-06-05 02:21:21 +0300

    a lot of work

--- mission_parameters.hpp
+++ mission_parameters.hpp
@@ -1,5 +1,5 @@
 // Размер зоны для игроков на время подготовки
-prepareZoneSize = 150;
+prepareZoneSize = 300;
 
 // Техника указанных сторон не может двигаться во время подготовки
 prepareVehicleFreeze[] = {"BLU_F", "OPF_F", "IND_F", "IND_G_F", "CIV_F"};
@@ -7,11 +7,11 @@ prepareVehicleFreeze[] = {"BLU_F", "OPF_F", "IND_F", "IND_G_F", "CIV_F"};
 blueforSide = "WEST";
 opforSide =  "EAST";
 sideSupremacy = 5; // Завершение по превосходству одной стороны над другой
-BFLoss = 15; // Завершение по проценту потерь стороны BLUEFOR
-OFLoss = 15; // Завершение по проценту потерь стороны OPFOR
+BFLoss = 10; // Завершение по проценту потерь стороны BLUEFOR
+OFLoss = 10; // Завершение по проценту потерь стороны OPFOR
 
 // Для Воскресных Игр (0 - нет, 1 - да)
-sundayGames = 0;
+sundayGames = 1;
 
 // Разрешить использовать T-Cube Motion Tracker (Fireteam HUD)? (0 - нет, 1 - да)
 fireteamHUD = 1;
